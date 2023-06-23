c493f405608afa8a6acd5b8e42899bacc3531123
Octavio Llanillo <llanillo4@hotmail.com>
2023-06-23 10:28:09 -0300

    Inicia proyecto

---
+++ src/main/java/com/dim/GestorEstacionesApp.java
@@ -0,0 +1,13 @@
+package com.dim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEstacionesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorEstacionesApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/dim/gestorestaciones/GestorEstacionesAppTests.java
@@ -0,0 +1,13 @@
+package com.dim.gestorestaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEstacionesAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
